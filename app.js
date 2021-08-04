45ca51612af71d0fb51703c363d08e0ca880437a
niranjan-gurung <nimagurung1@yahoo.com>
2021-08-04 13:14:29 +0100

    basic 16x16 grid create

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello!");
+const GRID_SIZE = 16;
+const parent = document.getElementById('container');
+
+/* SETUP 16X16 GRID: */
+for (let i = 0; i < GRID_SIZE; i++) {
+  for (let j = 0; j < GRID_SIZE; j++) {
+    const newdiv = document.createElement('div');
+    newdiv.style.cssText = 'border: 1px solid #ED6A5A; padding: 10px;';
+    parent.appendChild(newdiv);
+  }
+}

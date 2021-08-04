9b654d4ce17f05cddc51ecbee9b76b04b944dde9
niranjan-gurung <nimagurung1@yahoo.com>
2021-08-04 17:53:31 +0100

    fixed grid + hover effect when cursing indidividual box

--- app.js
+++ app.js
@@ -6,17 +6,26 @@
  * Cadet Blue: #5CA4A9
  * Alabaster: #E6EBE0
  */
-
-/* implement display: grid for the drawing area instead of flexbox! */
-
 const GRID_SIZE = 16;
-const parent = document.getElementById('container');
+const playGround = document.getElementById('container');
 
 /* SETUP 16X16 GRID: */
 for (let i = 0; i < GRID_SIZE; i++) {
   for (let j = 0; j < GRID_SIZE; j++) {
     const newdiv = document.createElement('div');
-    newdiv.style.cssText = 'border: 1px solid #ED6A5A; padding: 10px;';
-    parent.appendChild(newdiv);
+    newdiv.classList.toggle('box');
+    newdiv.style.cssText = 'border: 1.5px solid #ED6A5A;';
+    playGround.appendChild(newdiv);
   }
 }
+
+const grid = document.querySelectorAll('#container .box');
+
+grid.forEach((box) => {
+  box.addEventListener('mouseover', () => {
+    box.style.cssText = 'background-color: #9BC1BC';
+  });
+  box.addEventListener('mouseout', () => {
+    box.style.cssText = 'border: 1.5px solid #ED6A5A;';
+  });
+});

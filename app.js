f8b4878f7b2e3e01a34698b3ebe3f140001a444f
niranjan-gurung <nimagurung1@yahoo.com>
2021-08-05 12:23:53 +0100

    added clear button: resets grid back to original state

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
  * Alabaster: #E6EBE0
  */
 const GRID_SIZE = 16;
-const playGround = document.getElementById('container');
+const playGround = document.getElementById('playGround');
 
 /* SETUP 16X16 GRID: */
 for (let i = 0; i < GRID_SIZE; i++) {
@@ -19,13 +19,18 @@ for (let i = 0; i < GRID_SIZE; i++) {
   }
 }
 
-const grid = document.querySelectorAll('#container .box');
-
+const grid = document.querySelectorAll('#playGround .box');
+const clearBtn = document.getElementById('clear');
+/* DRAWING ON GRID: */
 grid.forEach((box) => {
   box.addEventListener('mouseover', () => {
     box.style.cssText = 'background-color: #9BC1BC';
   });
-  box.addEventListener('mouseout', () => {
+  clearBtn.addEventListener('click', () => {
     box.style.cssText = 'border: 1.5px solid #ED6A5A;';
   });
+  // box.addEventListener('mouseout', () => {
+  //   box.style.cssText = 'border: 1.5px solid #ED6A5A;';
+  // });
 });
+

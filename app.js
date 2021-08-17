3dd7f357b3566cd2e59f2243fab0c94b80eea789
niranjan-gurung <nimagurung1@yahoo.com>
2021-08-17 14:21:18 +0100

    implemented basic resize function, refactored code into reusable functions

--- app.js
+++ app.js
@@ -6,31 +6,66 @@
  * Cadet Blue: #5CA4A9
  * Alabaster: #E6EBE0
  */
-const GRID_SIZE = 16;
-const playGround = document.getElementById('playground');
 
-/* SETUP 16X16 GRID: */
-for (let i = 0; i < GRID_SIZE; i++) {
-  for (let j = 0; j < GRID_SIZE; j++) {
-    const newdiv = document.createElement('div');
-    newdiv.classList.toggle('box');
-    newdiv.style.cssText = 'background-color: white';
-    playGround.appendChild(newdiv);
+const playGround = document.getElementById('playground');
+const clearBtn   = document.querySelector('.clear');
+const resizeBtn  = document.querySelector('.resize');
+const colourBtn  = document.querySelector('.colour');  // need to implement
+
+function initGrid(GRID_SIZE) {
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      let newdiv = document.createElement('div');
+      newdiv.classList.toggle('box');
+      newdiv.style.cssText = 'background-color: white';
+      playGround.appendChild(newdiv);
+    }
   }
 }
 
-const grid = document.querySelectorAll('#playground .box');
+/* SETUP DEFAULT 16X16 GRID: */
+initGrid(16);
 
-const clearBtn = document.querySelector('.clear');
-const resizeBtn = document.querySelector('.resize');  // need to implement
-const colourBtn = document.querySelector('.colour');  // need to implement
-/* DRAWING ON GRID: */
-grid.forEach((box) => {
-  box.addEventListener('mouseover', () => {
-    box.style.cssText = 'background-color: black';
+// grid gets all individual divs(boxes) as a nodelist:
+let grid = document.querySelectorAll('#playground .box');
+/* DRAW ON MOUSE HOVER: */
+function drawGrid() {
+  grid.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+      box.style.cssText = 'background-color: black';
+    });
+    clearBtn.addEventListener('click', () => {
+      box.style.cssText = 'background-color: white';
+    });
   });
-  clearBtn.addEventListener('click', () => {
-    box.style.cssText = 'background-color: white';
-  });
+}
+
+drawGrid();
+
+/* TODO: 
+ * resizing should appropriately change pixel size of each single div,
+ * so that the size of the entire grid stays the same,
+ * but its pixel density inside is either increased or decreased. 
+ */
+
+resizeBtn.addEventListener('click', () => {
+  const newGridSize = prompt('Enter a new grid size: ');
+  // if cancel is clicked: 
+  if (newGridSize == "" || newGridSize == null) 
+    return;
+  else {
+    /* If valid resize value is entered:
+     * re-initialise with new grid size,
+     * re-select new amount of nodelist of divs,
+     * set grid template col+rows to new grid values,
+     * clear everything previously drawn on screen,
+     * redraw blank grid with new size.  
+     */
+    initGrid(newGridSize);    
+    grid = document.querySelectorAll('#playground .box');
+    document.getElementById("playground").style.gridTemplateColumns = (`repeat(${newGridSize}, 35px)`);
+    document.getElementById("playground").style.gridTemplateRows    = (`repeat(${newGridSize}, 35px)`);
+    grid.forEach((box) => box.style.cssText = 'background-color: white');
+    drawGrid();
+  }
 });
-
